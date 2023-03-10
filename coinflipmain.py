f98b304ebee55ac0301fa5f0b630c23980117b37
pogdogpy <101480097+pogdogpy@users.noreply.github.com>
2023-03-10 14:42:33 +0000

    Create

---
+++ coinflipmain.py
@@ -0,0 +1,19 @@
+#coinflip cuz bored
+import time
+import random
+
+
+coin = random.randint(1,2)
+
+
+heads = ("1")
+tails = ("2")
+print(coin)
+if coin == (1):
+    print("The Coin Landed On Heads")
+else:
+    print("The Coin Landed On Tails")
+
+
+
+#made by pogdog and prolly gonna be remade by kdot227 in like 24 hrs
